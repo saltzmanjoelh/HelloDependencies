796e1eaf57b72035cc6b22e989f71956fd965b31
Joel Saltzman <talk@joelsaltzman.com>
2018-07-17 23:45:36 -0700

    Initial commit

---
+++ App/App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Joel Saltzman on 7/17/18.
+//  Copyright © 2018 Joel Saltzman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
